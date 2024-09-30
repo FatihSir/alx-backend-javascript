ac7afacdc1580a7d23a0dc83a25676373899904d
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-09-30 22:29:35 +0300

    Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,11 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* eslint-dis no-unused_vars */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
-  /* eslint-enable no-unused-vars */
+  /* eslint-en no-unused-vars */
 
   return [task, task2];
 }
