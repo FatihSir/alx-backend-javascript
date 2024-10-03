19a3925608a36a4bbf528b8fc5863dad8459d767
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-10-03 20:46:55 +0300

    Task 5 fixes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,29 +1,25 @@
 class Building {
   constructor(sqft) {
-    // Ensure that the class inheriting from Building overrides evacuationWarningMessage
-    if (new.target !== Building) {
+    if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error('Classes extending Building must implement evacuationWarningMessage method');
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
       }
     }
     this.sqft = sqft;
   }
 
   /**
-   * Sets the square footage of the building
-   * @param {Number} sqft - The square footage to set
+   * @param {Number} sqft
    */
   set sqft(sqft) {
     if (typeof sqft !== 'number') {
-      throw new TypeError('Square footage (sqft) must be a number');
+      throw new TypeError('sqft nust be a number');
     }
     this._sqft = sqft;
   }
 
-  /**
-   * Gets the square footage of the building
-   * @returns {Number} - The square footage of the building
-   */
   get sqft() {
     return this._sqft;
   }
