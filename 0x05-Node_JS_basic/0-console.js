46581259d24f148083c69121e38865f0e05d5137
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-11-27 00:16:57 +0300

    some fixes

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,17 @@
+/**
+ * Logs a message to the STDOUT.
+ * @param {string} content - The message to printed.
+ * @returns {void}
+ */
+// function
 function displayMessage(content) {
   console.log(content);
 }
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
+module.exports = displayMessage;
