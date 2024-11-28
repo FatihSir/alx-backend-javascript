6afd808648954b41000f0fc9982f4b9135ad22e7
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-11-29 02:01:11 +0300

    Task 7

---
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -0,0 +1,35 @@
+const { expect } = require('chai');
+
+describe('Testing numbers', () => {
+  it('8 is equal to 8', () => {
+    expect(8 === 8).to.be.true;
+  });
+
+  it('9 is equal to 9', () => {
+    expect(9 === 9).to.be.true;
+  });
+
+  it.skip('2 is equal to 5', () => {
+    expect(2 === 5).to.be.true;
+  });
+
+  it('10 is equal to 10', () => {
+    expect(10 === 10).to.be.true;
+  });
+
+  it('11 is equal to 11', () => {
+    expect(11 === 11).to.be.true;
+  });
+
+  it('12 is equal to 12', () => {
+    expect(12 === 12).to.be.true;
+  });
+
+  it('13 is equal to 13', () => {
+    expect(13 === 13).to.be.true;
+  });
+
+  it('14 is equal to 14', () => {
+    expect(14 === 14).to.be.true;
+  });
+});
