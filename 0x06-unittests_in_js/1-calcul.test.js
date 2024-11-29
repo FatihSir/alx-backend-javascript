080e0caaff52179dae3cc43b7afe6af0838e5778
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-11-29 15:28:41 +0300

    Task 1 test fixes

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,45 +4,45 @@ const calculateNumber = require('./1-calcul');
 describe('calculateNumber', () => {
   describe('operation == "ADD"', () => {
     it('adds two small positive numbers', () => {
-      assert.strictEqual(calculateNumber('ADD', 1.4, 2.6), 4);
+      assert.strictEqual(calculateNumber('SUM', 1.4, 2.6), 4);
     });
 
     it('adds one negative and one positive number', () => {
-      assert.strictEqual(calculateNumber('ADD', -1.5, 3.2), 2);
+      assert.strictEqual(calculateNumber('SUM', -1.5, 3.2), 2);
     });
 
     it('adds two negative numbers', () => {
-      assert.strictEqual(calculateNumber('ADD', -1.7, -2.8), -5);
+      assert.strictEqual(calculateNumber('SUM', -1.7, -2.8), -5);
     });
 
     it('adds zero and a positive number', () => {
-      assert.strictEqual(calculateNumber('ADD', 0.0, 4.9), 5);
+      assert.strictEqual(calculateNumber('SUM', 0.0, 4.9), 5);
     });
 
     it('adds zero and a negative number', () => {
-      assert.strictEqual(calculateNumber('ADD', 0.0, -3.6), -4);
+      assert.strictEqual(calculateNumber('SUM', 0.0, -3.6), -4);
     });
   });
 
   describe('operation == "DIFFERENCE"', () => {
     it('subtracts two positive numbers', () => {
-      assert.strictEqual(calculateNumber('DIFFERENCE', 7.2, 3.6), 3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 7.2, 3.6), 3);
     });
 
     it('subtracts a positive number from a negative number', () => {
-      assert.strictEqual(calculateNumber('DIFFERENCE', -5.8, 3.1), -9);
+      assert.strictEqual(calculateNumber('SUBTRACT', -5.8, 3.1), -9);
     });
 
     it('subtracts a negative number from a positive number', () => {
-      assert.strictEqual(calculateNumber('DIFFERENCE', 6.7, -2.3), 9);
+      assert.strictEqual(calculateNumber('SUBTRACT', 6.7, -2.3), 9);
     });
 
     it('subtracts zero from a number', () => {
-      assert.strictEqual(calculateNumber('DIFFERENCE', 4.5, 0.0), 5);
+      assert.strictEqual(calculateNumber('SUBTRACT', 4.5, 0.0), 5);
     });
 
     it('subtracts a number from zero', () => {
-      assert.strictEqual(calculateNumber('DIFFERENCE', 0.0, -2.8), 3);
+      assert.strictEqual(calculateNumber('SUBTRACT', 0.0, -2.8), 3);
     });
   });
 
